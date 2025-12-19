e6df1f55b76eb7ac493bf3d82153c32645428748
MetaMsa <mserhataslan@hotmail.com>
2025-12-19 21:57:03 +0300

    swagger error fixed

--- lib/swagger.ts
+++ lib/swagger.ts
@@ -1,13 +1,14 @@
+import path from "path";
 import { createSwaggerSpec } from "next-swagger-doc";
 
 export const getSwaggerSpec = () =>
-    createSwaggerSpec({
-        apiFolder: "app/api",
-        definition: {
-            openapi: "3.0.0",
-            info: {
-                title: "API",
-                version: "1.0.0"
-            }
-        }
-    });
+  createSwaggerSpec({
+    apiFolder: path.join(process.cwd(), "app/api"),
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "API",
+        version: "1.0.0",
+      },
+    },
+  });
