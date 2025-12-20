82289d8525b953a49de7b0016da5721639ac63bc
MetaMsa <mserhataslan@hotmail.com>
2025-12-20 16:24:31 +0300

    swaggger updated

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,5 @@
 import type { NextConfig } from "next";
 
-import { writeFileSync } from 'fs';
-import { createSwaggerSpec } from 'next-swagger-doc';
-
-const spec = createSwaggerSpec({
-  apiFolder: 'app/api',
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API',
-      version: '1.0.0',
-    },
-  },
-});
-
-writeFileSync('public/swagger.json', JSON.stringify(spec, null, 2));
-
 const nextConfig: NextConfig = {
   /* config options here */
 };
