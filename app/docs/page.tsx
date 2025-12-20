69dd5d310eb61f7804e81eff729fcb65398c81b5
MetaMsa <mserhataslan@hotmail.com>
2025-12-20 16:01:07 +0300

    swaggger updated

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -2,5 +2,5 @@ import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 export default function Docs() {
-    return <SwaggerUI url="/swagger.json" />;
+    return <SwaggerUI url="/swagger.json" persistAuthorization={true} />;
 }
