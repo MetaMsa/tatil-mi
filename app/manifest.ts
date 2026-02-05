8db2c5650948c9e2f6cab1300aa3149d13711fc2
MetaMsa <mserhataslan@hotmail.com>
2026-02-05 18:04:50 +0300

    added manifest.json

---
+++ app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Tatil Mi?",
+    short_name: "Tatil Mi?",
+    description: "Kar tatili duyuruları",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
+}
