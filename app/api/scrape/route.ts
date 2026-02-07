6fea38efa0558297367ec734705723057814fa94
MetaMsa <mserhataslan@hotmail.com>
2026-02-07 22:23:30 +0300

    nitter instance self host

--- app/api/scrape/route.ts
+++ app/api/scrape/route.ts
@@ -20,7 +20,7 @@ export async function GET(req: NextRequest) {
       let results = await scrapeOfficial(cityName);
 
       if (!results || results.length === 0) {
-        if(processed.length % 20 === 0) {
+        if(processed.length % 20 === 0 && processed.length !== 0) {
           await new Promise((resolve) => setTimeout(resolve, 60000));
         }
         results = await scrapeSocial(cityAccount);
